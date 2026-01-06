c622af2f3f5ac20e9e484d30948e93af57e0f2b4
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-06 20:25:24 +0500

    123

---
+++ Tyuiu.PyrkinAA.Sprint3.Task7.V6.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.PyrkinAA.Sprint3.Task7.V6.Lib
+{
+    public class DataService : ISprint3Task7V6
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] valueArray = new double[len];
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double denominator = 4 * x - 0.5;
+                double y;
+
+                if (Math.Abs(denominator) < 1e-12)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = (3 * Math.Cos(x)) / denominator + Math.Sin(x) - 5 * x - 2;
+                }
+
+
+                y = Math.Round(y, 2);
+                valueArray[count] = y;
+                count++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.PyrkinAA.Sprint3.Task7.V6/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.PyrkinAA.Sprint3.Task7.V6.Lib; 
+using System;
+
+namespace Tyuiu.PyrkinAA.Sprint3.Task7.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("*******************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:            *");
+            Console.WriteLine("*******************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            double[] valueArray = ds.GetMassFunction(startValue, stopValue);
+            int len = valueArray.Length;
+
+            Console.WriteLine("*******************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                  *");
+            Console.WriteLine("*******************************");
+
+            Console.WriteLine("+-----------+-----------+");
+            Console.WriteLine("|     x     |   F(x)    |");
+            Console.WriteLine("+-----------+-----------+");
+
+            for (int i = 0; i < len; i++)
+            {
+                int x = startValue + i;
+                Console.WriteLine("| {0,5}     | {1,9:F2} |", x, valueArray[i]);
+            }
+
+            Console.WriteLine("+-----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+}
