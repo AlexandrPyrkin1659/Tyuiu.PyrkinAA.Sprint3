b9294ee934acb53b8537d2f34aeef05b0fad7a2e
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-06 18:54:48 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint3.Task6.V7.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PyrkinAA.Sprint3.Task6.V7.Lib;
+namespace Tyuiu.PyrkinAA.Sprint3.Task6.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 17;
+            int stopValue = 26;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 344;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
