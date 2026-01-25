ebe4fae6e2bf679d5aa419f7f8b36aba78442067
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-25 23:43:23 +0500

    1

--- Tyuiu.PyrkinAA.Sprint3.Task5.V17.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint3.Task5.V17.Lib/DataService.cs
@@ -8,19 +8,18 @@ namespace Tyuiu.PyrkinAA.Sprint3.Task5.V17.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             double SumSumSeries = 0;
+            double xSquared = Math.Pow(x, 2);
 
-            
+           
             for (int i = startValue1; i <= stopValue1; i++)
             {
                 
                 for (int k = startValue2; k <= stopValue2; k++)
                 {
-                    SumSumSeries += Math.Sin(k);
+                  
+                    SumSumSeries += Math.Sin(k + xSquared);
                 }
             }
-
-            
-            SumSumSeries += Math.Pow(x, 2);
 
             return Math.Round(SumSumSeries, 3);
         }
